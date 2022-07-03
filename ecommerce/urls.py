565fb013c0429a64dd634fa41d9006c74057a706
Jose Carlos <josecarlossneider@gmail.com>
2022-07-03 15:55:27 -0500

    configuracion heroku 3.0

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -11,6 +11,4 @@ urlpatterns = [
     url(r'^producto/',include(('apps.producto.urls','apps.producto'),namespace='producto')),
     url(r'^',include(('apps.empresa.urls','apps.empresa'),namespace='inicio'))
 ]+ static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
-if settings.DEBUG:
-    from django.conf.urls.static import static
-    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+
